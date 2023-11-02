bc9ee72721b7063d0a10a4ad23eae3c2e153d95c
gschram <giovani.schram@digio.com.br>
2023-11-02 19:43:10 -0300

    New account settings.

--- app/jwt.js
+++ app/jwt.js
@@ -3,7 +3,7 @@ import jsonwebtoken from "jsonwebtoken";
 export default {
 
     sign: (payload, signerParams) => new Promise(async (resolve, reject) => {
-        jsonwebtoken.sign(payload, signerParams.privateKey, { algorithm: signerParams.algorithm, expiresIn: process.env.JWT_EXPIRATION }, function(error, token) {
+        jsonwebtoken.sign(payload, signerParams.key, { algorithm: signerParams.algorithm, expiresIn: process.env.JWT_EXPIRATION }, function(error, token) {
             if (error) {
                 console.error(`Error: ${error}`);
                 reject(error);
